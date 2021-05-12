676d8fb2ae1f5ef8973acf1b6448d46a7d353a62
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-12 14:31:31 +0200

    use solidity type().min and type().max for SafeCast checks

--- test/utils/math/SafeCast.test.js
+++ test/utils/math/SafeCast.test.js
@@ -28,14 +28,14 @@ contract('SafeCast', async (accounts) => {
       it(`reverts when downcasting 2^${bits} (${maxValue.addn(1)})`, async function () {
         await expectRevert(
           this.safeCast[`toUint${bits}`](maxValue.addn(1)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an uint${bits}`,
         );
       });
 
       it(`reverts when downcasting 2^${bits} + 1 (${maxValue.addn(2)})`, async function () {
         await expectRevert(
           this.safeCast[`toUint${bits}`](maxValue.addn(2)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an uint${bits}`,
         );
       });
     });
@@ -102,28 +102,28 @@ contract('SafeCast', async (accounts) => {
       it(`reverts when downcasting -2^${bits - 1} - 1 (${minValue.subn(1)})`, async function () {
         await expectRevert(
           this.safeCast[`toInt${bits}`](minValue.subn(1)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an int${bits}`,
         );
       });
 
       it(`reverts when downcasting -2^${bits - 1} - 2 (${minValue.subn(2)})`, async function () {
         await expectRevert(
           this.safeCast[`toInt${bits}`](minValue.subn(2)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an int${bits}`,
         );
       });
 
       it(`reverts when downcasting 2^${bits - 1} (${maxValue.addn(1)})`, async function () {
         await expectRevert(
           this.safeCast[`toInt${bits}`](maxValue.addn(1)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an int${bits}`,
         );
       });
 
       it(`reverts when downcasting 2^${bits - 1} + 1 (${maxValue.addn(2)})`, async function () {
         await expectRevert(
           this.safeCast[`toInt${bits}`](maxValue.addn(2)),
-          `SafeCast: value doesn't fit in ${bits} bits`,
+          `SafeCast: value doesn't fit in an int${bits}`,
         );
       });
     });
